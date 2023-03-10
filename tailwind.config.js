85e402417fd9a56364eaeb68d7b35487a38fe192
OSHIN <izkp44@gmail.com>
2023-03-11 01:07:32 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    screen: { mobile: '375px', desktop: '1440px' },
+    fontFamily: {
+      manrope: ['Manrope']
+    },
+    extend: {
+      colors: {
+        bgWeb: 'hsl(218, 23%, 16%)',
+        bgAdvice: 'hsl(217, 19%, 24%)',
+        lightgreen: 'hsl(150, 100%, 66%)',
+        quote: 'hsl(193, 38%, 86%)'
+      }
+    }
   },
   plugins: []
 }
